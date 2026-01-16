df40d8901ff54d52a543ff1d27910d3bd7ac9712
nikhil misal <nik0misal@gmail.com>
2026-01-16 18:21:29 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),
+    email VARCHAR(100),
+    website VARCHAR(255),
+    gender VARCHAR(6),
+    comment VARCHAR(100)
+);
