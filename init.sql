966c9ffe210c20a4f404a3854320d35644e14b1e
nikhil misal <nik0misal@gmail.com>
2026-01-17 08:23:19 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -9,3 +9,34 @@ CREATE TABLE users (
     gender VARCHAR(6),
     comment VARCHAR(100)
 );
+
+
+
+
+
+
+
+
+USE FCT;                                              # Selects the FCT database to run the queries on
+
+CREATE TABLE users (                                 # Creates a table named users
+    id INT PRIMARY KEY AUTO_INCREMENT,               # Unique ID for each user, auto-generated
+    name VARCHAR(20),                                # Stores the user name
+    email VARCHAR(100),                              # Stores the user email address
+    website VARCHAR(255),                            # Stores the user website URL
+    gender VARCHAR(6),                               # Stores the user gender
+    comment VARCHAR(100)                             # Stores user comments or feedback
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
